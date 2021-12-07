7b957cc2034956e9b67eea61d111a967a919b62d
Serg I <iserg009@yandex.ru>
2021-12-07 20:32:36 +0300

    Добавлен пока пустой класс сервиса статей.

---
+++ src/main/java/ru/siv/notes/service/NotesService.java
@@ -0,0 +1,15 @@
+package ru.siv.notes.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.siv.notes.config.SharedResources;
+
+@Service
+@Slf4j
+public class NotesService {
+
+  @Autowired
+  private SharedResources res;
+
+}
