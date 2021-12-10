bec892fca8d206503e1cdd9bbbc68d6c741e3317
Serg I <iserg009@yandex.ru>
2021-12-10 15:30:01 +0300

    Создана возможность использовать мультиязычность. Все слова и тексты находящиеся на страницах вынесены в отдельный конфигурационный файл. Создан дополнительно конфигурационный файл для английского языка.

--- src/main/java/ru/siv/notes/config/MvcConfig.java
+++ src/main/java/ru/siv/notes/config/MvcConfig.java
@@ -1,8 +1,15 @@
 package ru.siv.notes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -13,4 +20,24 @@ public class MvcConfig implements WebMvcConfigurer {
     registry.addViewController("/login").setViewName("login");
   }
 
+  // Настройки для мультиязычности.
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.ENGLISH);
+    return slr;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
 }
